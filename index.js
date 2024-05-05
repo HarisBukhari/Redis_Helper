c43670f34e84a8507ca4de30311f1dbdc6f79ef7
Syed Haris Bukhari <45495476+HarisBukhari@users.noreply.github.com>
2024-05-05 21:24:15 +0500

    basic code

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const aixos = require('axios')
+const Redis = require("ioredis");
+const redis = new Redis();
+const app = express()
+app.get('/', async (req, res) => {
+  const cacheValue = await redis.get('data')
+  if(cacheValue) return res.json(JSON.parse(cacheValue))
+  const {data} = await aixos.get("https://jsonplaceholder.typicode.com/todos/")
+  await redis.set('data',JSON.stringify(data))
+  await redis.expire('data',30)
+  return res.json(data)
+})
+
+app.listen('3000', () => {
+  console.log('Listening on port 3000')
+})
+
